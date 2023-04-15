4975e5e9b1aae88950fc73c65721502d130160f7
Adriel Eusta <55837766+aniusta@users.noreply.github.com>
2023-04-15 14:02:44 +0800

    Initial Commit

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>mcl</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        fieldset{
+            display: flex;
+            justify-content: center;
+            border: none;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Calculator</h1>
+        </div>
+        <p>
+                <asp:Label ID="Label1" runat="server" Text="First Number">
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox></asp:Label>
+                </p>
+        <p>
+                <asp:Label ID="Label2" runat="server" Text="Second Number">
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></asp:Label>
+                </p>
+        <p>
+                <asp:Label ID="Label3" runat="server" Text="Second Number">
+                    <asp:TextBox ID="TextBox3" runat="server" disabled></asp:TextBox></asp:Label>
+                </p>
+        <p>
+                <asp:Button runat="server" id="btnAdd" Text="Add" OnClick="btnAdd_Click" />
+                <asp:Button runat="server" id="btnSubtract" Text="Subtract" OnClick="btnSubtract_Click" />
+            <asp:Button runat="server" id="btnDivide" Text="Divide" OnClick="btnDivide_Click" />
+            <asp:Button runat="server" id="btnMultiply" Text="Multiply" OnClick="btnMultiply_Click" />
+            </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public bool IsDigit(string first, string second)
+    {
+        bool digitsOnly = first.All(char.IsDigit) && second.All(char.IsDigit);
+
+        return digitsOnly;
+    }
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        bool x = IsDigit(TextBox1.Text, TextBox2.Text);
+        if (x == true)
+        {
+            int firstnum = Convert.ToInt32(TextBox1.Text);  
+            int secondnum = Convert.ToInt32(TextBox2.Text);
+            int sum = firstnum + secondnum;
+            TextBox3.Text = sum.ToString();
+        }
+        else
+        {
+            TextBox3.Text = "Input must be int";
+        }
+
+    }
+
+    protected void btnSubtract_Click(object sender, EventArgs e)
+    {
+        bool x = IsDigit(TextBox1.Text, TextBox2.Text);
+        if (x == true)
+        {
+            int firstnum = Convert.ToInt32(TextBox1.Text);
+            int secondnum = Convert.ToInt32(TextBox2.Text);
+            int sum = firstnum - secondnum;
+            TextBox3.Text = sum.ToString();
+        }
+        else
+        {
+            TextBox3.Text = "Input must be int";
+        }
+
+    }
+
+    protected void btnDivide_Click(object sender, EventArgs e)
+    {
+        bool x = IsDigit(TextBox1.Text, TextBox2.Text);
+        if (x == true)
+        {
+            int firstnum = Convert.ToInt32(TextBox1.Text);
+            int secondnum = Convert.ToInt32(TextBox2.Text);
+            int sum = firstnum / secondnum;
+            TextBox3.Text = sum.ToString();
+        }
+        else
+        {
+            TextBox3.Text = "Input must be int";
+        }
+
+    }
+
+    protected void btnMultiply_Click(object sender, EventArgs e)
+    {
+        bool x = IsDigit(TextBox1.Text, TextBox2.Text);
+        if (x == true)
+        {
+            int firstnum = Convert.ToInt32(TextBox1.Text);
+            int secondnum = Convert.ToInt32(TextBox2.Text);
+            int sum = firstnum * secondnum;
+            TextBox3.Text = sum.ToString();
+        }
+        else
+        {
+            TextBox3.Text = "Input must be int";
+        }
+
+    }
+}
